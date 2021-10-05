0b0e1c958b04103031794d325e4b19affd192c29
Zara Cooper <zramcooper@gmail.com>
2021-10-05 22:16:36 +0300

    Add blog support

--- scully.scully-portfolio.config.ts
+++ scully.scully-portfolio.config.ts
@@ -4,5 +4,11 @@ export const config: ScullyConfig = {
   projectName: "scully-portfolio",
   outDir: './dist/static',
   routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
   }
 };
