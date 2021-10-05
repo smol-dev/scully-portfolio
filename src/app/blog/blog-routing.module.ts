782821f97bcf7fc5bac21934397464bfc89d1197
Zara Cooper <zramcooper@gmail.com>
2021-10-05 22:27:30 +0300

    Add the blog landing component

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogLandingComponent } from './blog-landing/blog-landing.component';
 
 import { BlogComponent } from './blog/blog.component';
 
 const routes: Routes = [
+  { path: '', component: BlogLandingComponent },
   {
     path: ':slug',
     component: BlogComponent,
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog/blog.component';
+import { BlogLandingComponent } from './blog-landing/blog-landing.component';
 
 @NgModule({
-  declarations: [BlogComponent],
+  declarations: [BlogComponent, BlogLandingComponent],
   imports: [CommonModule, BlogRoutingModule, ScullyLibModule],
 })
 export class BlogModule { }
