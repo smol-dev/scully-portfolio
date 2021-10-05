ed7bfd54c74a59ac5f5aa7a92901abf9e4078d7a
Zara Cooper <zramcooper@gmail.com>
2021-10-05 21:27:16 +0300

    Update readme Change project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/scratch-portfolio'),
+      dir: require('path').join(__dirname, './coverage/scully-portfolio'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- scully.scratch-portfolio.config.ts
+++ scully.scratch-portfolio.config.ts
@@ -1,7 +1,7 @@
 import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
   projectRoot: "./src",
-  projectName: "scratch-portfolio",
+  projectName: "scully-portfolio",
   outDir: './dist/static',
   routes: {
   }
