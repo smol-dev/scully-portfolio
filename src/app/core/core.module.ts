28d2787f4aed4221fd92c38fec86ed14876b1fc4
Zara Cooper <zramcooper@gmail.com>
2021-10-05 21:36:49 +0300

    Add core module imports

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    NgbModule,
+    HttpClientModule
   ]
 })
 export class CoreModule { }
